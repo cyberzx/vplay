47eabf4d0fc0e6f335c62c5f1c1e7ce0a87731e2
cyberzx <cyberzx@gmail.com>
2017-02-20 13:19:10 +0300

    vapi

---
+++ src/vulkan_api.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifndef VK_USE_PLATFORM_XCB_KHR
+#define VK_USE_PLATFORM_XCB_KHR
+#endif
+
+#include  <vulkan/vulkan.h>
+#include  <vulkan/vk_platform.h>
+#include  <vulkan/vulkan.hpp>
+
